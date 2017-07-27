91cb7bf678122736488eaa35fd28dde835cb119a
yumeng.zou <yumeng.zou@wustl.edu>
2017-07-27 18:27:10 -0500

    onType finished!

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Social Network Analysis on Xie Lansheng's diary, a 19th-century diary of Canton 
 
 Goal: Explore the social interactions and geographical activities documented in the diary
 
-Tools: Excel for data entry, Python for data management, Gephi for visualization, R for network analysis
+Tools: Excel for data entry, Python-pandas for data management/data analysis, Gephi for visualization, R-igraph for network analysis
--- core/Analysis/onType.py
+++ core/Analysis/onType.py
@@ -3,6 +3,8 @@ Created on Jul 25, 2017
 
 @author: yumeng.zou
 '''
+# -*- coding: utf-8 -*-
+
 import pandas as pd
 import numpy as np
 import os
@@ -13,6 +15,8 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.widgets import Button, CheckButtons
 
 os.chdir("/Users/yumeng.zou/Google Drive/Freshyear/Summer/Research/Diary/")
+plt.rcParams['backend']='GTK'
+plt.rcParams['font.family']='Microsoft YaHei'
 
 diary=pd.read_csv("csv/Diary.csv")
 
@@ -22,8 +26,10 @@ class ClusterByType():
     colName=""
     CusType=pd.DataFrame()
     pivot=pd.DataFrame()
+    labels=[]
+    fig=plt.figure(figsize=(9,6))
     switch=pd.Series([False,False,True,True,True,True,True])
-        
+
     def init(self,colName):
         ClusterByType.colName=colName
         EventCus=diary[['Event',colName]].dropna()
@@ -38,12 +44,12 @@ class ClusterByType():
     def draw(self):
         mat=ClusterByType.pivot.replace(np.nan,0).as_matrix()
         clusters=KMeans(n_clusters=ClusterByType.n).fit(mat)
+        ClusterByType.labels=clusters.labels_
         pca=skl.PCA(n_components=3).fit(mat)
         x,y,z=zip(*pca.transform(mat))
-           
-        fig=plt.figure(figsize=(9,6))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(y,x,z,c=clusters.labels_,s=50,alpha=0.5)
+        
+        ax = ClusterByType.fig.add_subplot(111, projection='3d')
+        points=ax.scatter(y,x,z,c=ClusterByType.labels,s=50,alpha=0.5,picker=3)
         ax.set_title('Clustering Places based on the Type of events they appear in')
         
         # axes labels
@@ -53,12 +59,17 @@ class ClusterByType():
         ax.set_zlabel('{:.2f}%'.format(zl*100))
         
         # Button: regenerate graph
-        axdraw = plt.axes([0.81, 0.05, 0.1, 0.075])
+        axdraw = plt.axes([0.85, 0.03, 0.1, 0.06])
         bdraw=Button(axdraw,'Draw')
         bdraw.on_clicked(ClusterByType.draw)
         
+        # Button: close graph and store csv
+        axdraw = plt.axes([0.85, 0.85, 0.1, 0.06])
+        bclose=Button(axdraw,'Close')
+        bclose.on_clicked(ClusterByType.ClosenSave)
+        
         # Check Button: filter features
-        rax = plt.axes([0.05, 0.5, 0.1, 0.3])
+        rax = plt.axes([0.02, 0.4, 0.1, 0.35])
         ClusterByType.switch.index=ClusterByType.CusType['Type'].value_counts()[:7].index
         check = CheckButtons(rax, ClusterByType.switch.index, ClusterByType.switch)
         def checkFilter(label):
@@ -74,20 +85,39 @@ class ClusterByType():
         check.on_clicked(checkFilter)
         
         # mouse event: full information of each point
+        def onPick(event):
+            
+            if event.artist != points:
+                return True
+                
+            N = len(event.ind)
+            if not N:
+                return True
+            
+            i=0
+            for dataidx in event.ind:
+                if i==2:
+                    break
+                x,y,z=event.artist._offsets3d
+                t=str(ClusterByType.pivot.reset_index().loc[dataidx].dropna())
+                ax.text(x[dataidx],y[dataidx],z[dataidx],str(t),fontsize=8)
+                i+=1
+            plt.show()
+            return True
         
+        ClusterByType.fig.canvas.mpl_connect('pick_event', onPick)
         
         plt.show()
         
-        
- 
+    def ClosenSave(self):
+        result=pd.DataFrame({'cluster':ClusterByType.labels,'label':ClusterByType.pivot.index}).sort_values(by='cluster',ascending=False)
+        result.to_csv("csv/placeCluster.csv",index=False,encoding='utf-8')
+        plt.close()
+
+
+
 g = ClusterByType()
 g.init('Place')
 g.draw()
 
- 
-# plt.savefig("Graph/placeType.png")
-# plt.close()
- 
-# result=pd.DataFrame({'cluster':cluster.labels_,'label':pivot.index}).sort_values(by='cluster',ascending=False)
-# result.to_csv("csv/placeCluster.csv",index=False,encoding='utf-8')
 
